fcb01c41c7a29a8b1345f8acdb73f1c674d26e10
Wanja Wilson <wanjaswilly@gmail.com>
2026-02-13 16:09:38 +0300

    added a save method to contact message service to save user message

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,52 +3,58 @@
 namespace App\Controllers;
 
 use App\Models\SiteStat;
+use App\Services\ContactMessageService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class HomeController
+class HomeController extends BaseController
 {
+
+    private ContactMessageService $contactMessageService;
+
     public function about(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/about.twig');
+        return $this->render($request, $response, 'pages/about.twig');
     }
 
 
     public function home(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/home.twig');
+        return $this->render($request, $response, 'pages/home.twig');
     }
 
 
     public function contact(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/contact.twig');
+        return $this->render($request, $response, 'pages/contact.twig');
     }
 
 
     public function terms(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/terms.twig');
+        return $this->render($request, $response, 'pages/terms.twig');
     }
 
 
     public function privacy(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/privacy.twig');
+        return $this->render($request, $response, 'pages/privacy.twig');
     }
 
     
 
     public function developer(Request $request, Response $response): Response
     {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'pages/developer.twig');
+        return $this->render($request, $response, 'pages/developer.twig');
+    }
+
+     /**
+     * Display contact page
+     */
+    public function saveContact(Request $request, Response $response)
+    {
+        return $this->render($request, $response,'pages/contact-success.twig', $this->contactMessageService->saveMessage($request));
     }
 
 }
--- app/Services/ContactMessageService.php
+++ app/Services/ContactMessageService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace   App\Services;
+namespace App\Services;
 
 use App\Exceptions\ValidationException;
 use App\Models\ContactMessage;
@@ -10,38 +10,61 @@ use Psr\Http\Message\ServerRequestInterface;
 class ContactMessageService
 {
 
+    public function saveMessage(ServerRequestInterface $request): array
+    {
+        $data = $request->getParsedBody();
+        if (empty($data['fullname']) || empty($data['phonenumber']) || empty($data['message'])) {
+            throw new ValidationException("Incomplete Form", ['error' => 'Please fill in all required fields']);
+        }
+
+        # save the message/inquiry
+        ContactMessage::create([
+            'fullname' => $data['fullname'],
+            'phonenumber' => $data['phonenumber'],
+            'emailaddress' => $data['email'] ?? null,
+            'message' => $data['message'],
+        ]);
+
+        $_SESSION['success'] = 'Your message has been received by our team. We will get back to you with 48 workig hours';
+        return [
+            'status' => 200,
+            'message' => sprintf("Thank you %s for leaving your message. Our team will get back to you with 48 workig hours", $data['fullname'])
+        ];
+
+    }
+
     public function allMessages(): array
     {
         return [
             'messages' => ContactMessage::latest()->get(),
         ];
     }
-    public function singleMessageData(int $messageId):array
-    {        
-        if(!$message = ContactMessage::with('repliedby')->find($messageId)){
-            throw new ValidationException('Message not Found',['error' => "Contact message not found, try again"]);
+    public function singleMessageData(int $messageId): array
+    {
+        if (!$message = ContactMessage::with('repliedby')->find($messageId)) {
+            throw new ValidationException('Message not Found', ['error' => "Contact message not found, try again"]);
         }
         return [
             'message' => $message,
         ];
     }
-    public function saveReply(ServerRequestInterface $request, int $messageId):array
+    public function saveReply(ServerRequestInterface $request, int $messageId): array
     {
         $data = $request->getParsedBody();
-        if(empty($data['reply'])){
+        if (empty($data['reply'])) {
             throw new ValidationException('Empty Message Reply', ['error' => "reply to a contact message cannot be empty"]);
         }
 
-        if(!$message = ContactMessage::with('repliedby')->find($messageId)){
-            throw new ValidationException('Message not Found',['error' => "Contact message not found, try again"]);
+        if (!$message = ContactMessage::with('repliedby')->find($messageId)) {
+            throw new ValidationException('Message not Found', ['error' => "Contact message not found, try again"]);
         }
         $message->reply = $data['reply'];
         $message->repliedat = Carbon::now();
         $message->user_id = $_SESSION['user_id'];
-        if($message->save()){
+        if ($message->save()) {
             $_SESSION['success'] = "Your reply was saved successfully";
-        }else{
-            throw new ValidationException('Error while saving reply',['error' => "An error occured while saving your reply"]);
+        } else {
+            throw new ValidationException('Error while saving reply', ['error' => "An error occured while saving your reply"]);
         }
 
         return [
