e3868c5810e21f800da6e7175a3e30a23e59431d
Wanja Wilson <wanjaswilly@gmail.com>
2026-02-19 09:40:41 +0300

    introduced Payment callback error exception to log the errors in database and error log

---
+++ app/Exceptions/PaymentCallbackErrorException.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * This exception is thrown if an unauthenticated user tries to access a resource.
+ * they are redirected to login page with the given error
+ * 
+ */
+class PaymentCallbackErrorException extends Exception
+{
+/**
+ * Summary of __construct
+ * @param string $message - error message
+ * @param array $errors - an array of all errors 
+ * @param int $status - HTTP code
+ * @return array array [<string, mixed>]
+ */
+public function __construct(
+        string $message = "Application error",
+        public array $errors = [],
+        public int $status = 400,
+    ) {
+        parent::__construct($message, $status);
+
+        # Log the exception with the user who caused the exception
+        $errorMessageArray = [
+            'message' => $message,
+            'errors' => $errors,
+            'status' => $status,
+        ];
+        error_log(json_encode($errorMessageArray));
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'message' => $this->getMessage(),
+            'errors' => $this->errors,
+            'status' => $this->status,
+        ];
+    }
+
+}
